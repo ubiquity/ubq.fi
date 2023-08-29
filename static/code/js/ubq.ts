0bf5f9949de5098faad9236723cffc149206e0a5
アレクサンダー.eth <gpg@pavlovcik.com>
2023-08-30 07:30:36 +0900

    feat: copywriting

--- static/code/js/ubq.ts
+++ static/code/js/ubq.ts
@@ -39,3 +39,12 @@ while (x--) {
 
 import { grid } from "./the-grid";
 grid(document.getElementById("grid-dynamic"));
+
+document.addEventListener("DOMContentLoaded", function () {
+  const button = document.getElementById("hamburger-button");
+  const menu = document.getElementById("menu");
+
+  button.addEventListener("click", function () {
+    menu.classList.toggle("open");
+  });
+});
