2db06d5ae62a381eb913e6ca0a8a7e6ece105af4
アレクサンダー.eth <gpg@pavlovcik.com>
2024-03-03 08:55:49 +0900

    chore: update build scripts

--- build/esbuild-build.ts
+++ build/esbuild-build.ts
@@ -1,8 +1,7 @@
 import esbuild from "esbuild";
-const typescriptEntries = ["static/code/js/ubq.ts", "static/code/js/sine.ts"];
+const typescriptEntries = ["static/main.ts", "static/code/js/ubq.ts", "static/code/js/sine.ts"];
 const cssEntries = ["static/code/css/ubq.css", "static/code/css/proxima.css"];
 const entries = [...typescriptEntries, ...cssEntries];
-
 export const esBuildContext: esbuild.BuildOptions = {
   sourcemap: true,
   entryPoints: entries,
