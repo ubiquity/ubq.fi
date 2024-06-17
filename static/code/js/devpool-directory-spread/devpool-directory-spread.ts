c95530f9a015aec2d20a45e28cfeb3806181259f
アレクサンダー.eth <gpg@pavlovcik.com>
2024-06-17 15:50:09 +0900

    fix: devpool analytics

--- static/code/js/devpool-directory-spread/devpool-directory-spread.ts
+++ static/code/js/devpool-directory-spread/devpool-directory-spread.ts
@@ -27,7 +27,36 @@ export function devPoolSpread() {
     .then((totalRewards) => {
       const totalRewardsElement = document.getElementById("fetch-total-rewards-target");
       if (!totalRewardsElement) throw new Error("No total rewards element");
-      totalRewardsElement.innerText = `Earn $${totalRewards} USD in task rewards now.`;
+      const htmlBuffer = [
+        `<h3>${totalRewards.rewards.total} USD in rewards across ${totalRewards.tasks.total} projects.</h3>`,
+        `
+        <table id="analytics">
+          <thead>
+            <tr>
+              <th></th>
+              <th>Available</th>
+              <th>Ongoing</th>
+              <th>Completed</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Rewards</td>
+              <td>$${totalRewards.rewards.notAssigned}</td>
+              <td>$${totalRewards.rewards.assigned}</td>
+              <td>$${totalRewards.rewards.completed}</td>
+            </tr>
+            <tr>
+              <td>Tasks</td>
+              <td>${totalRewards.tasks.notAssigned}</td>
+              <td>${totalRewards.tasks.assigned}</td>
+              <td>${totalRewards.tasks.completed}</td>
+            </tr>
+          </tbody>
+        </table>
+      `,
+      ].join("\n");
+      totalRewardsElement.innerHTML = htmlBuffer;
     })
     .catch((error) => {
       console.error("Error fetching total rewards:", error);
--- static/code/js/devpool-directory-spread/fetch-total-rewards.ts
+++ static/code/js/devpool-directory-spread/fetch-total-rewards.ts
@@ -1,7 +1,22 @@
 export async function fetchTotalRewards() {
-  const response = await fetch("https://raw.githubusercontent.com/ubiquity/devpool-directory/development/total-rewards.txt");
+  const response = await fetch("https://raw.githubusercontent.com/ubiquity/devpool-directory/development/total-rewards.json");
   if (!response.ok) {
     throw new Error(`HTTP error! status: ${response.status}`);
   }
-  return await response.text();
+  return (await response.json()) as TotalRewards;
+}
+
+interface TotalRewards {
+  rewards: {
+    notAssigned: number;
+    assigned: number;
+    completed: number;
+    total: number;
+  };
+  tasks: {
+    notAssigned: number;
+    assigned: number;
+    completed: number;
+    total: number;
+  };
 }
