485e1cd72c3dcc3bd3857c4c5538212b2949f58c
アレクサンダー.eth <gpg@pavlovcik.com>
2024-06-17 21:45:38 +0900

    fix: remove unused code now iframe is removed

--- static/code/js/devpool-directory-spread/devpool-directory-spread.ts
+++ static/code/js/devpool-directory-spread/devpool-directory-spread.ts
@@ -1,28 +1,6 @@
 import { fetchTotalRewards } from "./fetch-total-rewards";
 
 export function devPoolSpread() {
-  document.addEventListener("DOMContentLoaded", () => {
-    const iframe = document.querySelector("#DevPool iframe") as HTMLIFrameElement;
-    if (!iframe) throw new Error("No iframe element");
-
-    const observer = new IntersectionObserver((entries) => {
-      if (entries[0].isIntersecting) {
-        if (!iframe) throw new Error("No iframe element");
-        iframe.src = "https://work.ubq.fi/";
-        observer.disconnect();
-      }
-    }, {});
-    observer.observe(iframe);
-
-    iframe.addEventListener(
-      "touchmove",
-      (e) => {
-        e.preventDefault();
-      },
-      { passive: false }
-    );
-  });
-
   fetchTotalRewards()
     .then((totalRewards) => {
       const totalRewardsElement = document.getElementById("fetch-total-rewards-target");
